5afd308ab3e2a1ef4fedfbdf7899eb2a433f5405
Viktor <golubenkovvs@gmail.com>
2022-03-23 10:47:49 +0300

    initial state

---
+++ FlyingSquares/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlyingSquares
+//
+//  Created by Viktor Golubenkov on 23.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlyingSquares/FlyingSquaresApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlyingSquaresApp.swift
+//  FlyingSquares
+//
+//  Created by Viktor Golubenkov on 23.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct FlyingSquaresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
