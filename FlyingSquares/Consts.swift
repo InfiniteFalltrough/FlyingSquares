fb9f7e185ffd602764162f237c24f5fc40ab69b9
InfiniteFalltrough <golubenkovvs@gmail.com>
2025-07-25 17:33:56 +0700

    updated and organized Consts.swift

--- FlyingSquares/Consts.swift
+++ FlyingSquares/Consts.swift
@@ -7,17 +7,34 @@
 
 import SwiftUI
 
-// screen size
-let width: CGFloat = UIScreen.main.bounds.width
-let height: CGFloat = UIScreen.main.bounds.height
-
-//random X/Y
-func randomIntX() -> Int {
-    let randomX = Int.random(in: -15..<15)
-    return randomX
+struct Consts {
+    static let screenWidth: CGFloat = UIScreen.main.bounds.width
+    static let screenHeight: CGFloat = UIScreen.main.bounds.height
+    
+    struct Physics {
+        static let impulseRange: ClosedRange<Int> = -15...15
+        static let nodeSize: CGSize = CGSize(width: 75, height: 75)
+        static let physicsBodyRadius: CGFloat = screenWidth / 10
+    }
+    
+    struct Timing {
+        static let gyroUpdateInterval: TimeInterval = 0.1 / 60
+        static let initialDelay: TimeInterval = 0.25
+        static let forcePushInterval: TimeInterval = 7.5
+    }
+    
+    struct Collision {
+        static let firstCategory: UInt32 = 1 << 0
+        static let edgeCategory: UInt32 = 1 << 31
+    }
 }
-func randomIntY() -> Int {
-    let randomY = Int.random(in: -15..<15)
-    return randomY
+
+// MARK: - Random Generation
+extension Consts {
+    static func randomImpulse() -> CGVector {
+        let x = Int.random(in: Physics.impulseRange)
+        let y = Int.random(in: Physics.impulseRange)
+        return CGVector(dx: x, dy: y)
+    }
 }
 
